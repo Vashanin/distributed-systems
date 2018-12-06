6939a02cb99487854247a5dc8156692c5fd7cae7
Vladyslav Shanin <vashanin7@gmail.com>
2018-12-06 16:21:26 +0100

    Update

---
+++ configs.py
@@ -0,0 +1,7 @@
+nodes = [
+    {"host": None, "port": 6666, "username": None, "password": None},
+    {"host": None, "port": 6666, "username": None, "password": None}
+]
+
+timeout = 1.0
+retries = 5
